12342ea6ee1f6bc51f2173e2e5dad64cd507f978
Mohammed Dawood <mahmedtony73@gmail.com>
2022-03-26 23:03:40 +0200

    Adjusting the Custom Hook Logic
    use call back and enhance the parameters passing to custom hooks

---
+++ src/hooks/use-http.js
@@ -0,0 +1,34 @@
+import { useCallback, useState } from "react";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      // call back function provided by the component
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
+
+  return { isLoading, error, sendRequest }; // return an object of hooks inside custom hook
+};
+
+export default useHttp;
